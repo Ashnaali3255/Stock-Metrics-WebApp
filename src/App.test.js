6c07c60fbb4c7f03df5b83b3d8ea15cc35046ba9
Ashnaali3255 <ashnaalipk@gmail.com>
2023-06-07 14:08:41 +0500

    Update data

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without error', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
--- src/redux/CompaniesList.js
+++ src/redux/CompaniesList.js
@@ -1,60 +1,3 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-
-// const FETCH_COMPANIES_LIST_REQUEST = 'FETCH_COMPANIES_LIST_REQUEST';
-// const FETCH_COMPANIES_LIST_SUCCESS = 'FETCH_COMPANIES_LIST_SUCCESS';
-// const FETCH_COMPANIES_LIST_FAILURE = 'FETCH_COMPANIES_LIST_FAILURE';
-
-// const fetchCompaniesList = createAsyncThunk(
-//   'companies/fetchCompaniesList',
-//   async () => {
-//     const url = 'https://mattermarkraygorodskijv1.p.rapidapi.com/getCompaniesList';
-//     const options = {
-//       method: 'POST',
-//       headers: {
-//         'X-RapidAPI-Key': '032b57fca6mshe34316a66b67d92p1e8431jsn944052e12892',
-//         'X-RapidAPI-Host': 'MattermarkraygorodskijV1.p.rapidapi.com',
-//       },
-//     };
-
-//     try {
-//       const response = await fetch(url, options);
-//       const result = await response.text();
-//       console.log(result);
-//       return result;
-//     } catch (error) {
-//       console.error(error);
-//       throw error;
-//     }
-//   },
-// );
-
-// // Reducer function
-// const companiesReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case FETCH_COMPANIES_LIST_REQUEST:
-//       return {
-//         ...state,
-//       };
-//     case FETCH_COMPANIES_LIST_SUCCESS:
-//       return {
-//         ...state,
-//         companiesList: action.payload,
-//         error: null,
-//       };
-//     case FETCH_COMPANIES_LIST_FAILURE:
-//       return {
-//         ...state,
-//         companiesList: null,
-//         error: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export { fetchCompaniesList };
-// export default companiesReducer;
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const GET_COMPANIES = 'companies/companies/GET_COMPANIES';
@@ -97,7 +40,6 @@ export const getCompanyDetails = createAsyncThunk(
   async (args, { dispatch }) => {
     const response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${args}?apikey=7463b68c7759d533ae3d84f1c5ef122b`);
     const data = await response.json();
-    // console.log(`data: ${data}`);
 
     dispatch({ type: GET_COMPANIEY_DETAILS, companies: data[0] });
   },
